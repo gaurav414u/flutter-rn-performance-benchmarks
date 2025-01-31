715904c471d025b28629cbd9c5b04fbe74a25e28
Gaurav Bhola <gaurav414u@gmail.com>
2025-01-30 22:58:10 -0800

    Add android_list_view_perf representing list view impelementation on android native. Match with flutter as much as possible w.r.t. everything.

---
+++ list_view_with_images/android_list_view_perf/app/src/main/java/com/example/android_list_view_perf/MainActivity.kt
@@ -0,0 +1,139 @@
+package com.example.android_list_view_perf
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestScreen()
+        }
+    }
+}
+
+data class Item(val index: Int, val color: Color)
+
+@Composable
+fun TestScreen() {
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val data = remember { generateData() }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            state = listState,
+            verticalArrangement = Arrangement.spacedBy(20.dp) // Adjust spacing as needed
+        ) {
+            items(data) { item ->
+                Cell(item)
+            }
+        }
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    listState.animateScrollToItem(1000)
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text("Start Scrolling")
+        }
+    }
+}
+
+@Composable
+fun Cell(item: Item) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val rotation by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(10000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(item.color)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("file:///android_asset/images/${item.index % 20}.jpeg")
+                .size(100, 100) // Equivalent to cacheHeight and cacheWidth in Flutter
+                .build(),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .size(100.dp)
+                .aspectRatio(1f)
+        )
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("file:///android_asset/images/${item.index % 20}.jpeg")
+                .size(100, 100) // Equivalent to cacheHeight and cacheWidth in Flutter
+                .build(),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .size(100.dp)
+                .graphicsLayer(rotationZ = rotation)
+                .aspectRatio(1f)
+        )
+        Text(
+            text = item.index.toString(), modifier = Modifier
+                .align(Alignment.CenterVertically)
+        )
+    }
+}
+
+fun generateData(): List<Item> {
+    return List(1001) { index -> Item(index, randomColor()) }
+}
+
+fun randomColor(): Color {
+    return Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f)
+}
+
